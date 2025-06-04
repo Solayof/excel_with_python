851e91f83d661b8ec24a5ce081e6cacb3f5a89bb
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-04 13:04:38 +0100

    create student model

---
+++ models/portal/student.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""student model
+"""
+from sqlalchemy import Column, ForeignKey, JSON, String
+from sqlalchemy.orm import relationship
+from models.portal.admission import Admission
+
+
+class Student(Admission):
+    """student model
+    
+    Usage: student = Student(
+            username="jesa",
+            email="asd@gdha",
+            admission_no="231",
+            arm="A"
+        )
+        other parameters are optional
+
+    Args:
+        Admission (_type_): admission class
+    """    
+    __tablename__ = "students"
+    extend_existing = True
+    _id = Column(
+        String(36),
+        ForeignKey('admission_register._id'),
+        primary_key=True
+        )
+    classroom_id = Column(String(36), ForeignKey("classes.id"))
+    classroom = relationship(
+        "Class",
+        foreign_keys=[classroom_id],
+        back_populates="students",
+        uselist=False
+        )
+    subjects = relationship("Subject",
+        foreign_keys="[Subject.student_id]",
+        back_populates="student",
+        uselist=True)
+    
+
+    def to_dict(self):
+        """dictionary representation of class instance
+
+        Returns:
+            _type_: dict
+        """        
+        new_dict = self.__dict__.copy()
+        new_dict.pop("_sa_instance_state", None)
+        
+        new_dict["created_at"] = self.created_at.isoformat()
+
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        
+        
+        classroom = self.classroom
+        new_dict["classroom_id"] = classroom.id if classroom else None
+        
+                
+        return new_dict
