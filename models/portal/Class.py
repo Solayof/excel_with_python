aac773fe948abd9940ea8ef96050f0afd665e4e9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-04 14:45:30 +0100

    upate class model

--- models/portal/Class.py
+++ models/portal/Class.py
@@ -2,9 +2,12 @@
 """class model
 """
 from datetime import datetime
+from openpyxl import load_workbook
+from openpyxl.utils import coordinate_to_tuple, get_column_letter
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from models.base import Base
+from models.engine.dbSheet import Workbook
 from models.baseModel import BaseModel
 
 class Class(BaseModel, Base):
@@ -41,8 +44,11 @@ class Class(BaseModel, Base):
             className = className.upper() + arm.upper()
             code = className.replace(" ", "-")
 
-            yr = datetime.now().strftime("%y")
-            kwargs["code"] = code + "-" + f"20{yr}-20{int(yr) + 1}"
+            yr = int(datetime.now().strftime("%y"))
+            mth = datetime.now().strftime("%m")
+            if int(mth) < 8:
+                yr = yr - 1
+            kwargs["code"] = code + "-" + f"20{yr}-20{yr + 1}"
             kwargs["className"] = className
             kwargs["arm"] = arm
             super().__init__(*args, **kwargs)
@@ -73,3 +79,51 @@ class Class(BaseModel, Base):
         
         
         return new_dict
+    
+    def getSheet(self):
+        filePath = f"{self.className[:3]}.xlsx"
+        worksheet = Workbook(filePath=filePath)
+        worksheet.open_session()
+
+        return worksheet
+    
+    def generateSheet(self):
+        students = self.students
+        worksheet = self.getSheet()
+        worksheet.open_session()
+
+        stdcell = "b4"
+        for student in students:
+            worksheet.writeCell(cell=stdcell, value=student.fullName)
+            subjects = student.subjects
+            stdrow, stdcol = coordinate_to_tuple(stdcell)
+            admnocell = f"{get_column_letter(stdcol + 1)}{stdrow}"
+            worksheet.writeCell(cell=admnocell, value=student.admission_no)
+            gendercell = f"{get_column_letter(stdcol + 2)}{stdrow}"
+            worksheet.writeCell(cell=gendercell, value=student.gender)
+            classcell = f"{get_column_letter(stdcol + 3)}{stdrow}"
+            worksheet.writeCell(cell=classcell, value=student.classroom.className)
+            for subject in subjects:
+                subCell = worksheet.getSubjectCell(subject=subject.name)
+                _, subcol = coordinate_to_tuple(subCell)
+
+                CAcell = f"{get_column_letter(subcol)}{stdrow}"
+                worksheet.writeCell(cell=CAcell, value=subject.CA)
+                examcell = f"{get_column_letter(subcol + 1)}{stdrow}"
+                worksheet.writeCell(cell=examcell, value=subject.examScore)
+                scdtermcell = f"{get_column_letter(subcol + 3)}{stdrow}"
+                worksheet.writeCell(cell=scdtermcell, value=subject.secondTermScore)
+                frttermcell = f"{get_column_letter(subcol + 4)}{stdrow}"
+                worksheet.writeCell(cell=frttermcell, value=subject.firstTermScore)
+                
+
+
+            stdcell = f"{get_column_letter(stdcol)}{stdrow + 1}"
+
+        worksheet.saveWorkbook(self.code)
+
+    @property
+    def sheetSubjects(self):
+        return self.getSheet().dbSubjects()
+
+
