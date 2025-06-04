66adb4512e4e777e2c81e315fe49db823ea0b07c
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-06-04 14:45:08 +0100

    update dbsheet

--- models/engine/dbSheet.py
+++ models/engine/dbSheet.py
@@ -1,34 +1,54 @@
-from openpyxl.utils import coordinate_to_tuple, get_column_letter, load_workbook
+from openpyxl import load_workbook
+from openpyxl.utils import coordinate_to_tuple, get_column_letter
 
 
 class Workbook:
     __workbook = None
+    filePath = ''
     
+ 
+    def __init__(self, *args, **kwargs):
+        self.filePath = kwargs.pop("filePath", None)
     
-    @classmethod
-    def init(cls, filePath=None):
-        cls.__workbook = load_workbook(filePath)
+    def open_session(self):
+        if not self.__workbook:
+            self.__workbook = load_workbook(self.filePath)
 
-     @classmethod   
-    def open_session(cls, filePath=None):
-        if not cls.__workbook:
-            cls.init(filePath)
+       
+    def getDbsheet(self, sheetName="Db"):
+        return self.__workbook[sheetName]
 
+    def saveWorkbook(self, fileName):
+        if self.__workbook is None:
+            return False
+        self.__workbook.save(f"{fileName}.xlsx")
+        return True
 
-     @classmethod       
-    def getDbsheet(cls, sheetName="Db"):
-        return cls.__workbook.get_sheet_by_name(sheetName)
-    @classmethod
-    def saveWorkbook(cls, fileName):
-        cls.__workbook.save(f"{fileName}.xlsx")
-        
-    @classmethod
-    def dbSubject(cls, sheetName="Db"):
+    def dbSubjects(self, sheetName="Db"):
         sub = []
         cell = "j2"
-        db = cls.getDbsheet(sheetName)
+        db = self.getDbsheet(sheetName)
         while db[cell].value:
             sub.append(db[cell].value)
             row, col = coordinate_to_tuple(cell)
             cell = f"{get_column_letter(col + 6)}{row}"
         return sub
+    
+    def writeCell(self, cell, value, sheetName="Db"):
+        sheet = self.getDbsheet(sheetName=sheetName)
+        sheet[cell] = value
+
+    def readCell(self, cell, sheetName="Db"):
+        sheet = self.getDbsheet(sheetName=sheetName)
+        return sheet[cell].value
+    
+    def getSubjectCell(self, subject, sheetName="Db"):
+        cell = "j2"
+        db = self.getDbsheet(sheetName)
+        while db[cell].value and db[cell].value != subject:
+            row, col = coordinate_to_tuple(cell)
+            cell = f"{get_column_letter(col + 6)}{row}"
+
+        if db[cell].value == subject:
+            return cell
+        
