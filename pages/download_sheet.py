bcb33497ffb792851189c15ac8702f89ef5d1f00
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-30 02:30:00 +0100

    feat: add download sheet functionality with class selection and file download

---
+++ pages/download_sheet.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+from datetime import datetime
+from io import BytesIO, StringIO
+import numpy as np
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from models.cache import (getClassrooms,
+                          getClassroom,
+                          getStudentById,
+                          getStudentsIdandNames,
+                          getclassSubjects,
+                          session_list)
+from models.portal.admission import Admission
+from models.portal.cache import current_session, current_term, term_list
+from models.portal.Class import Class
+
+from models.portal.student import Student
+from models.portal.subject import Subject
+
+from models.portal.user import User
+
+st.set_page_config(
+    page_title="Record Student Subject Score",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an app to manage student result"
+    }
+)
+st.title("Download Sheet")
+
+
+
+def download_sheet():
+    st.header("Download Sheet")
+    code = st.selectbox("Class", getClassrooms())
+    if code:
+        clss = Class.query.filter_by(code=code).one_or_none()
+
+        try:
+            with open(f"{clss.code}.xlsx", "rb") as file:
+                st.download_button(
+                    label=f"Download {clss.code}.xlsx file",
+                    data=file,
+                    file_name=f"{clss.code}.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"
+                )
+                
+        except FileNotFoundError:
+            st.error("Generate sheet for the class first")
