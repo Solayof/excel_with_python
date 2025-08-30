e27c1b0b281b9b1fd440d43dd7930545297dbac1
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-30 02:29:02 +0100

    feat: implement subject score recording with validation and user feedback

---
+++ pages/create_subject.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+from datetime import datetime
+from io import BytesIO, StringIO
+import numpy as np
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from models.cache import (getClassrooms,
+                          getClassroom,
+                          getStudentById,
+                          getStudentsIdandNames,
+                          getclassSubjects,
+                          session_list)
+from models.portal.admission import Admission
+from models.portal.cache import current_session, current_term, term_list
+from models.portal.Class import Class
+
+from models.portal.student import Student
+from models.portal.subject import Subject
+
+from models.portal.user import User
+
+st.set_page_config(
+    page_title="Record Student Subject Score",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an app to manage student result"
+    }
+)
+st.title("Record Student Subject Score")
+def create_subject():    
+    st.header("Upload Scores")
+    code = st.selectbox("Class", getClassrooms())
+    subjectname = None
+    name = None
+    if code:
+        fullNameId = getStudentsIdandNames(code)
+        name = st.selectbox("Name", fullNameId.keys())
+        term_lists = term_list()
+        term = st.selectbox("Term", term_lists, index=term_lists.index(current_term()))
+        if name and term:
+            student = Student.query.filter_by(id=fullNameId[name]).one_or_none()
+            subjectlist = [sub for sub in getclassSubjects(code) 
+                           if sub not in student.subject_recorded(term=term)]
+    
+            subjectname = st.selectbox("Subject", subjectlist)
+    ca = st.number_input("Continuous Assessment", 0, 30)
+    exam = st.number_input("Third Term Score (Just the exam score)", 0, 70)
+
+    if st.button("upload score") and name and subjectname and ca and exam:
+        mth = datetime.now().strftime("%m")
+        if term == "First Term" and mth not in ['09', '10', '11', '12']:
+            st.warning("You are recording first term score outside first term period")
+            return
+        if term == "Second Term" and mth not in ['01', '02', '03', '04']:
+            st.warning("You are recording second term score outside second term period")
+            return
+        if term == "Third Term" and mth not in ['05', '06', '07', '08']:
+            st.warning("You are recording third term score outside third term period")
+            return
+        subject = Subject()
+        subject.name = subjectname
+        subject.student_id = student.id
+        subject.CA = ca
+        subject.examScore = exam
+        subject.term = term
+        subject.session = current_session()
+        if not subject.student_id:
+            st.error("score can not be save, no student attached")
+        subject.save()
+        st.success("score save successfully")
+
+create_subject()
