43bcea3c39e2dbc27fab751f2cc2d34ccf4ecff4
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-30 02:25:38 +0100

    feat: add caching functions for classroom and student data retrieval

---
+++ models/cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import streamlit as st
+
+
+
+from models.portal.admission import Admission
+from models.portal.Class import Class
+
+from models.portal.student import Student
+from models.portal.subject import Subject
+
+from models.portal.user import User
+
+@st.cache_data(ttl=5000)
+def getClassrooms():
+    return Class.all()
+
+@st.cache_data(ttl=5000)
+def getStudentsIdandNames(code):
+    clss = Class.query.filter_by(code=code).one_or_none()
+    return clss.getStudentsIdandNames()
+
+@st.cache_data(ttl=5000)
+def getclassSubjects(code):
+     clss = Class.query.filter_by(code=code).one_or_none()
+     return clss.sheetSubjects
+
+@st.cache_data(ttl=60000)
+def getClassroom(code):
+    return Class.query.filter_by(code=code).one_or_none()
+
+@st.cache_data(ttl=30000)
+def getStudentById(id):
+    return Student.query.filter_by(id=id).one_or_none()
+
+@st.cache_data(ttl=30000)
+def session_list():
+    subjects = Subject.query.with_entities(Subject.session).distinct().all()
+    return [sub[0] for sub in subjects if sub[0]]
+
+st.cache_data(ttl=30000)
+def grade(score):
+                    if score >= 70: return "A"
+                    elif score >= 60: return "B"
+                    elif score >= 50: return "C"
+                    elif score >= 40: return "D"
+                    else: return "F"
