97692092c7999a1489934178689106766a1de838
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-30 02:30:27 +0100

    feat: add generate sheet functionality for recording student subject scores

---
+++ pages/generate_sheet.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from datetime import datetime
+from io import BytesIO, StringIO
+import numpy as np
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from models.cache import (getClassrooms,
+                          getClassroom,
+                          getStudentById,
+                          getStudentsIdandNames,
+                          getclassSubjects,
+                          session_list)
+from models.portal.admission import Admission
+from models.portal.cache import current_session, current_term, term_list
+from models.portal.Class import Class
+
+from models.portal.student import Student
+from models.portal.subject import Subject
+
+from models.portal.user import User
+
+st.set_page_config(
+    page_title="Record Student Subject Score",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an app to manage student result"
+    }
+)
+st.title("Generate Sheet")
+
+
+
+def generate_sheet():
+        code = st.selectbox("Class", getClassrooms())
+        if code:
+            clss = Class.query.filter_by(code=code).one_or_none()
+        if st.button("Generate Sheet") and clss:
+            clss.generateSheet()
+            st.success(f"sheet with file name: {code}.xlsx generated successfully")
+
+generate_sheet()
