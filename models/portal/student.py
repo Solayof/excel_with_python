2a784b1fd286940496e5d5e49a933c016d1a85f9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-09-16 10:05:17 +0100

    refactor: remove department relationship from Student model

--- models/portal/student.py
+++ models/portal/student.py
@@ -42,13 +42,7 @@ class Student(Admission):
         back_populates="student",
         uselist=True)
     
-    department_id = Column(String(36), ForeignKey("departments.id", ondelete="SET NULL"))
-    department = relationship(
-        "Department",
-        foreign_keys=[department_id],
-        back_populates="students",
-        uselist=False
-    )
+    
     
 
     def to_dict(self):
