a7b669e7fed86520024e4c70d87427c56c4324a9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-09-16 10:08:06 +0100

    feat: implement user authentication checks and update class creation logic

--- pages/create_class.py
+++ pages/create_class.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-from models.cache import getClassrooms, getClassroom, getStudentById, getStudentsIdandNames, getclassSubjects
+from models.cache import departs_name_with_id, getClassrooms, getClassroom, getStudentById, getStudentsIdandNames, getclassSubjects
 from models.portal.admission import Admission
 from models.portal.Class import Class
 from models.portal.department import Department
@@ -14,6 +14,13 @@ from models.portal.student import Student
 from models.portal.subject import Subject
 
 from models.portal.user import User
+from pages import session_auth
+
+current_user = session_auth.current_user()
+if not current_user:
+    st.switch_page("CHS_IGBOPE_PORTAL.py")
+if current_user.isAdmin() is False:
+    st.switch_page("CHS_IGBOPE_PORTAL.py")
 
 st.set_page_config(
     page_title="Create Student",
@@ -30,8 +37,12 @@ st.title("Add Class")
 def create_class():
     room = st.selectbox("classroom", ["JSS 1", "JSS 2", "JSS 3", 'SSS 1', 'SSS 2', 'SSS 3'])
     arm = st.selectbox("Arms", ["A", "B", "C", "D", "E"])
+    departs_dict = departs_name_with_id()
+    name = st.selectbox("Department", departs_dict.keys())
+    depart_id = departs_dict.get(name, None) if name else None
     if st.button('Add class') and arm and room:
         clss = Class(className=room, arm=arm)
+        clss.department_id = depart_id
         if  getClassroom(clss.code):
             st.warning(f"class {clss.code} exists")
             return
