97c135518a17b5eba542108853003f1cfadac7b0
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-12-02 06:47:55 +0000

    update create subject file

--- pages/create_subject.py
+++ pages/create_subject.py
@@ -61,7 +61,7 @@ def create_subject():
     
             subjectname = st.selectbox("Subject", subjectlist)
     ca = st.number_input("Continuous Assessment", 0, 30)
-    exam = st.number_input("Third Term Score (Just the exam score)", 0, 70)
+    exam = st.number_input("Examination Score", 0, 70)
 
     if st.button("upload score") and name and subjectname and ca and exam:
         mth = datetime.now().strftime("%m")
